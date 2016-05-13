4d0d86af97e84659a5b40a14c8d90d96e7dabf37
ChristianCruz <christian@simpleshape.me>
2016-05-13 14:59:09 -0500

    re-work controllers and actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  resources :products
+
+  resources :posts 
 
   devise_for :users
 
