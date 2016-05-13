5a5b85da2ff04641b354537fd16249c0f4dd112b
ChristianCruz <christian@simpleshape.me>
2016-05-13 15:30:47 -0500

    Adding more CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,35 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:product, :description))
+
+    #raise # This will short-circuit the method
+
+    if @post.save
+      flash[:notice] = "Post saved successfully!"
+      redirect_to @post
+    else
+      flash[:error] = " There was an error saving the post. Please try again!"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:product, :description))
+      flash[:notice] = "Post was updated"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error updating your post! Please try again!"
+      render :edit
+    end
   end
 end
